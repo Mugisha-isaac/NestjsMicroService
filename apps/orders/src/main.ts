baae28c4b8d903f39a5f0a7e31c667de40471bc1
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-04 12:47:06 +0200

    ft: get orders

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
+  console.log(`Listening on port ${configService.get('PORT')}`);
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderRequest } from './dto/create-order.request';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
   async createOrder(@Body() order: CreateOrderRequest) {
     return this.ordersService.createOrder(order);
+  }
+
+  @Get()
+  async getOrders() {
+    return this.ordersService.getOrders();
   }
 }
