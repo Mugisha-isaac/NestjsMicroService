c472fb13f72d43f51a91fbaaf7a2a048ea0d7aa6
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-07-15 15:11:55 +0200

    ft: add custom api response

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { CustomResponseInterceptor } from 'libs/interceptors/custom-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new CustomResponseInterceptor());
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT'));
 }
---
+++ libs/interceptors/custom-response.interceptor.ts
@@ -0,0 +1,44 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, map, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class CustomResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    const request = context.switchToHttp().getRequest();
+    const response = context.switchToHttp().getResponse();
+    const statusCode = response.statusCode;
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode,
+        message: statusCode >= 400 ? 'error' : 'success',
+        error: statusCode >= 400 ? response.message : null,
+        timestamp: Date.now(),
+        version: '1.0.0',
+        path: request.url,
+        data,
+      })),
+      catchError((err) => {
+        const statusCode = err instanceof HttpException ? err.getStatus() : 500;
+        const errorResponse = {
+          statusCode,
+          message: err.message || 'Internal Server Error',
+          error: err.name || 'Error',
+          timestamp: Date.now(),
+          version: '1.0.0',
+          path: request.url,
+          data: {},
+        };
+        return throwError(() => new HttpException(errorResponse, statusCode));
+      }),
+    );
+  }
+}
